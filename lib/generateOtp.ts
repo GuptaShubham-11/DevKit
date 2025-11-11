40a13da655e2c22bb1804674a1b2c07424461472
GuptaShubham-11 <shu919847@gmail.com>
2025-11-11 17:44:52 +0530

    feat(utils): add generate OTP utility (#6)

--- lib/generateOtp.ts
+++ lib/generateOtp.ts
@@ -1,8 +1,5 @@
 import crypto from 'crypto';
 
-// Helper to generate secure OTP
-function generateOtp(): string {
+export function generateOtp(): string {
   return crypto.randomInt(0, 1_000_000).toString().padStart(6, '0');
 }
-
-export { generateOtp };
