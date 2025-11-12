a50bff35a8da6e70c8057990b634a889209d28a7
Gupta Shubham <164208744+GuptaShubham-11@users.noreply.github.com>
2025-11-12 20:09:49 +0530

    refactor(utils): check user is admin (#26)
    Refactor user is admin utility.

--- lib/checkUserIsAdmin.ts
+++ lib/checkUserIsAdmin.ts
@@ -1,13 +1,11 @@
 import { User } from '@/models/user';
 
-async function checkUserIsAdmin(userId: string): Promise<boolean> {
+export async function checkUserIsAdmin(userId: string): Promise<boolean> {
   try {
     const user = await User.findById(userId);
     return user.isAdmin || false;
-  } catch (error) {
-    console.error('Error checking admin status:', error);
+  } catch {
+    // console.error('Error checking admin status:', error);
     return false;
   }
 }
-
-export { checkUserIsAdmin };
