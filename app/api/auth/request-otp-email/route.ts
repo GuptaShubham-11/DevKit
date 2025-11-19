f4550caf85ddd653f3558069dbac6f5ce521ab0a
GuptaShubham-11 <shu919847@gmail.com>
2025-11-19 16:35:25 +0530

    refactor(api): reset password email to request otp email

---
+++ app/api/auth/request-otp-email/route.ts
@@ -0,0 +1,70 @@
+import { User } from '@/models/user';
+import { sendEmail } from '@/lib/email';
+import { connectToDatabase } from '@/lib/db';
+import { generateOtp } from '@/lib/generateOtp';
+import { requestOtpSchema } from '@/validation/auth';
+import { NextRequest, NextResponse } from 'next/server';
+import { resetPasswordHtml } from '@/emails/resetPassword';
+import { emailVerificationHtml } from '@/emails/emailVerification';
+
+export async function PATCH(request: NextRequest) {
+  try {
+    const reqData = await request.json();
+
+    const validatedData = requestOtpSchema.safeParse(reqData);
+    if (!validatedData.success) {
+      return NextResponse.json(
+        { error: validatedData.error.issues[0].message },
+        { status: 400 }
+      );
+    }
+
+    const { email, type } = validatedData.data;
+    await connectToDatabase();
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return NextResponse.json(
+        {
+          message:
+            'If an account with that email exists, an OTP has been sent.',
+        },
+        { status: 200 }
+      );
+    }
+
+    const otp = generateOtp();
+    const isReseting = type === 'resetPassword';
+
+    // Save OTP and expiration
+    user.otp = otp;
+    user.otpExpiry = new Date(Date.now() + 15 * 60 * 1000); // 15 minutes expiry
+    await user.save({ validateBeforeSave: false });
+
+    await sendEmail({
+      emailAddress: email,
+      emailSubject: isReseting
+        ? 'DevKit - OTP for Password Reset'
+        : 'DevKit - OTP for Email Verification',
+      htmlText: isReseting
+        ? resetPasswordHtml(otp)
+        : emailVerificationHtml(otp),
+    });
+
+    return NextResponse.json(
+      {
+        message: 'If an account with that email exists, an OTP has been sent.',
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    // console.error('Error during forgot password process:', error);
+    return NextResponse.json(
+      {
+        error: 'Forgot password process failed!',
+        details: error,
+      },
+      { status: 500 }
+    );
+  }
+}
