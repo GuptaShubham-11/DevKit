3c98334ec3cc21ea0e107ce7fc53c4ae9222f4e4
GuptaShubham-11 <shu919847@gmail.com>
2025-12-05 17:16:59 +0530

    bugfix(api): fix imagekit public key

--- app/api/imagekit-auth/route.ts
+++ app/api/imagekit-auth/route.ts
@@ -2,9 +2,16 @@ import { getUploadAuthParams } from '@imagekit/next/server';
 
 export async function GET() {
   try {
+    if (
+      !process.env.IMAGEKIT_PRIVATE_KEY ||
+      !process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY
+    ) {
+      throw new Error('Imagekit keys are not set in environment variables!');
+    }
+
     const authenticationParameters = getUploadAuthParams({
       privateKey: process.env.IMAGEKIT_PRIVATE_KEY as string,
-      publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY as string,
+      publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY as string,
     });
 
     return Response.json({
