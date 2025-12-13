470878573e4b2edc1e0db7222cfc63bd18e2e825
Gupta Shubham <164208744+GuptaShubham-11@users.noreply.github.com>
2025-12-13 16:53:43 +0530

    feat(utils): add steps configuration for step indicator (#71)

---
+++ lib/small-utils/badge.ts
@@ -0,0 +1,19 @@
+import { Step } from '@/components/StepIndicator';
+
+export const steps: Step[] = [
+  {
+    id: 1,
+    title: 'Basic Info',
+    icon: 'Info',
+  },
+  {
+    id: 2,
+    title: 'Configuration',
+    icon: 'Setting',
+  },
+  {
+    id: 3,
+    title: 'Rewards',
+    icon: 'Gift',
+  },
+];
