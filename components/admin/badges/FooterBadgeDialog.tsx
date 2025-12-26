d4592a55316247425591807f3e8f518732e508a0
Gupta Shubham <164208744+GuptaShubham-11@users.noreply.github.com>
2025-12-26 15:39:12 +0530

    feat(components): add footer of BadgeDialog (#87)

---
+++ components/admin/badges/FooterBadgeDialog.tsx
@@ -0,0 +1,50 @@
+import { FooterBadgeDialogProps } from '@/types/small-types/badge';
+import CustomButton from '@/components/CustomButton';
+import { ArrowRight } from 'lucide-react';
+
+export function FooterBadgeDialog({
+  currentStep,
+  TOTAL_STEPS,
+  canProceedToNextStep,
+  isUploading,
+  loading,
+  isFormValid,
+  prevStep,
+  nextStep,
+  handleSubmit,
+  handleClose,
+}: FooterBadgeDialogProps) {
+  return (
+    <div className="flex items-center justify-between gap-2 mt-4">
+      <CustomButton
+        type="button"
+        variant="secondary"
+        onClick={currentStep === 1 ? handleClose : prevStep}
+        label={currentStep === 1 ? 'Cancel' : 'Back'}
+        disabled={loading || isUploading}
+        className="flex-1"
+      />
+
+      {currentStep < TOTAL_STEPS ? (
+        <CustomButton
+          type="button"
+          variant="primary"
+          onClick={nextStep}
+          label="Next"
+          icon={ArrowRight}
+          disabled={!canProceedToNextStep || isUploading}
+        />
+      ) : (
+        <CustomButton
+          type="button"
+          variant="primary"
+          onClick={handleSubmit}
+          label="Create Badge"
+          disabled={loading || !isFormValid || isUploading}
+          loading={loading}
+          shimmer
+        />
+      )}
+    </div>
+  );
+}
